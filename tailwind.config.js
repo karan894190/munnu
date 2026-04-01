1451e631dd2c9857220d98827e8d8468fa61c483
karan894190 <karan894190@gmail.com>
2026-04-01 07:51:45 +0530

    Add tailwind.config.js with custom colors and premium typography configuration

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'deep-black': '#000000',
+        'pure-white': '#FFFFFF',
+        'university-red': '#FF0000',
+        'royal-blue': '#0066CC',
+        'metallic-gold': '#FFD700',
+      },
+      fontFamily: {
+        'premium': ['"Your Font Name Here"', 'sans-serif'],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
